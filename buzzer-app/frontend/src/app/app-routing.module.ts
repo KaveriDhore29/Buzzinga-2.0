caea1c57e7ce7ae667ad2fa86f9764ae1a326b5b
Kaveri Dhore <dhorekaveri@gmail.com>
2025-12-23 20:13:55 +0530

    Leave button added

--- buzzer-app/frontend/src/app/app-routing.module.ts
+++ buzzer-app/frontend/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     component:HomeComponent
   },
   {
+    path:'landing-page',
+    component:HomeComponent
+  },
+  {
     path:'ask-room',
     component:AskRoomComponent
   },
--- buzzer-app/frontend/src/app/pages/main-room/main-room.component.ts
+++ buzzer-app/frontend/src/app/pages/main-room/main-room.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-room',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainRoomComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+  }
+
+   leaveRoom() {
+    this.router.navigate(['/landing-page']);
   }
 
 }
