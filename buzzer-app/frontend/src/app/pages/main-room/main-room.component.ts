f5a68fb3528d75d10be966b051d5ece8009e8ad8
Kaveri Dhore <dhorekaveri@gmail.com>
2025-12-24 23:53:02 +0530

    made backend setup,socket service in frontend and basic function of buzz worked

--- buzzer-app/frontend/src/app/pages/main-room/main-room.component.ts
+++ buzzer-app/frontend/src/app/pages/main-room/main-room.component.ts
@@ -1,20 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SocketService } from 'src/app/socket.service';
 
 @Component({
   selector: 'app-main-room',
-  templateUrl: './main-room.component.html',
-  styleUrls: ['./main-room.component.css']
+  templateUrl: './main-room.component.html'
 })
 export class MainRoomComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  roomId!: string;
+  username = 'User_' + Math.floor(Math.random() * 1000); // later from login
+  role = 'user';
+
+  members: any[] = [];
+  buzzes: any[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private socketService: SocketService,
+    private router:Router
+  ) {}
 
   ngOnInit(): void {
+    this.roomId = this.route.snapshot.paramMap.get('id')!;
+
+    this.socketService.joinRoom({
+      roomId: this.roomId,
+      name: this.username,
+      role: this.role
+    });
+
+    this.socketService.onMembersUpdated().subscribe((members:any) => {
+      this.members = members;
+    });
+
+    this.socketService.onBuzz().subscribe((buzz:any) => {
+      this.buzzes.unshift(buzz); // latest on top
+    });
   }
 
-   leaveRoom() {
+  buzz() {
+    this.socketService.buzz({
+      roomId: this.roomId,
+      name: this.username
+    });
+  }
+
+    leaveRoom() {
     this.router.navigate(['/landing-page']);
   }
-
 }
